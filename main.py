9abcc68d45311d3f3412d07186a5a55a2f0e4ec7
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-08-28 09:31:35 +0000

    rename generic variables to improve readability

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from collections import Counter
 
 def most_frequent(List): 
     occurence_count = Counter(List) 
-    return [line[0] for line in occurence_count.most_common(2)]
+    return [name_line[0] for name_line in occurence_count.most_common(2)]
 
-def remove_date(line):
-    if "Today" in line:
-        name_line = line.split("Today")
+def remove_date(line_to_remove):
+    if "Today" in line_to_remove:
+        name_line = line_to_remove.split("Today")
         name_line[1] = ""
-    elif "Yesterday" in line:
-        name_line = line.split("Yesterday")
+    elif "Yesterday" in line_to_remove:
+        name_line = line_to_remove.split("Yesterday")
         name_line[1] = ""
-    elif "/" in line:
-        name_line = [line[:-11], ""]
+    elif "/" in line_to_remove:
+        name_line = [line_to_remove[:-11], ""]
     else:
-        return line
+        return line_to_remove
     return name_line
 
 message  = open("message.txt", "r")
 
 seperate_message = message.readlines()
 
-for line in seperate_message:
-    current_line = remove_date(line)
+for name_line in seperate_message:
+    current_line = remove_date(name_line)
     if len(current_line) == 2:
-        for line in current_line:
-            print(f"{Fore.GREEN}{line}")
+        for name_line in current_line:
+            print(f"{Fore.GREEN}{name_line}")
     else:
-        print(f"{Fore.RESET}{str(line)}")
+        print(f"{Fore.RESET}{str(name_line)}")
